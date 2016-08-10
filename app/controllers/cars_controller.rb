b6af03f8eba0a4c9da25f90ff7c7503fa4dda2ae
Andrew Young <prince.of.utah@gmail.com>
2016-08-10 02:50:45 -0400

    added create and destroy code and routes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,45 +1,44 @@
 class CarsController < ApplicationController
 
 
-    def index
-      @cars = Car.all
-    end
+  def index
+    @cars = Car.all
+  end
 
-    def new
-      @car = Car.new
-    end
+  def new
+    @car = Car.new
+  end
 
-    def create
-      @car = Car.create!(car_params)
+  def create
+    @car = Car.create!(car_params)
+    redirect_to @car
+  end
 
-      redirect_to @car
-    end
+  def show
+    @car = Car.find(params[:id])
+  end
 
-    def show
-      @car = Car.find(params[:id])
-    end
+  def edit
+    @car = Car.find(params[:id])
+  end
 
-    def edit
-      @car = Car.find(params[:id])
-    end
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
 
-    def update
-      @car = Car.find(params[:id])
-      @car.update(car_params)
+    redirect_to @car
+  end
 
-      redirect_to @car
-    end
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
 
-    def destroy
-      @car = Car.find(params[:id])
-      @car.destroy
-
-      redirect_to cars_path
-    end
-
-    def car_params
-      params.require(:car).permit(:name, :neighborhood, :beers, :photo_url)
-    end
+  private
+  def car_params
+    params.require(:car).permit(:vehicle, :date, :mileage, :service, :cost, :estimate, :shop, :mechanic, :quality, :overall, :recommendations, :comments, :photo_url)
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :cars, only: [:index, :show]
+  resources :cars, only: [:index, :show, :new, :create, :destroy]
   # get 'cars' => 'cars#index'
   # get 'cars/:id' => 'cars#show'
 end
